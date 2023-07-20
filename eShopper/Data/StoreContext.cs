78465918aacd92209a7b83c11c47860650bf3307
sekhwelad <25sekhwelad@gmail.com>
2023-07-20 19:51:49 +0200

    Product Controller

---
+++ eShopper/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using eShopper.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShopper.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
